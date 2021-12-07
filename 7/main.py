9beeeb84096fdf45a04c622fe96c90facf7154b4
Kay Es <schmitteckertkay@freenet.de>
2021-12-07 08:37:48 +0100

    rename function

--- 7/main.py
+++ 7/main.py
@@ -4,11 +4,11 @@ with open('7/input.txt', 'r') as file:
 hmin = min(lines)
 hmax = max(lines)
 
-def get_position_with_minimum_fuel(sum_function):
+def get_position_with_minimum_fuel(sum_fuel):
     best_pos = -1
     best_fuel = float("inf")
     for x in range(hmin, hmax):
-        fuel = int(sum([sum_function(x, p) for p in lines]))
+        fuel = int(sum([sum_fuel(x, p) for p in lines]))
         if fuel < best_fuel:
             best_pos = x
             best_fuel = fuel
